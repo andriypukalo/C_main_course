26e7803d7ba7e5ea570749c398994d94aca3f139
Andriy <andriy.pukalo@gmail.com>
2018-11-22 21:31:12 +0200

    events

---
+++ SwitcherEvents/SwitcherEvents/Switcher/DataReader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switcher
+{
+    public class DataReader
+    {
+        
+
+        private string newString;
+        public string NewString// строка що вводиться
+        {
+            get
+            {
+                return newString;
+            }
+            set
+            {
+                newString = value;
+                OnStringChanged();// що відбувається при зміні данних в стрічці
+            }
+        }
+
+        public event System.EventHandler StringChanged;// подія на ввід строки
+
+        protected virtual void OnStringChanged()
+        {
+            if (StringChanged != null) StringChanged(this, EventArgs.Empty);
+        }
+
+        
+    }
+}
