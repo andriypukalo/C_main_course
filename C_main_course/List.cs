16924491b97a4dc92c35982a3a088bddcea693ad
Andriy <andriy.pukalo@gmail.com>
2018-11-22 19:23:10 +0200

    last commit

---
+++ C_main_course/List.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_main_course
+{
+
+    public class List<T>
+    {
+        private class Node
+        {
+            public T Item { get; set; }
+            public Node Prev { get; set; }
+            public Node(T item)
+            {
+                Item = item;
+            }
+        }
+
+        public List()
+        {
+        }
+
+        private Node Top
+        {
+            get;
+            set;
+        }
+
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        public void Add(T value)
+        {
+            Node node = new Node(value);
+            node.Prev = Top;
+            Top = node;
+            Count++;
+        }
+
+        public T Pop()
+        {
+
+            if (Top == null)
+                throw new Exception("List is empty");
+            
+                T res = Top.Item;
+                Top = Top.Prev;
+                Count--;
+
+            
+            return res;
+        }
+    }
+}
---
+++ C_main_course/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_main_course
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<String> MyList = new List<string>();
+            MyList.Add("Andriy");
+            MyList.Add("Serhiy");
+            MyList.Add("Ivan");
+
+
+
+            while (MyList.Count > 0)
+            {
+                Console.WriteLine(MyList.Pop());
+            }
+
+
+        }
+    }
+}
